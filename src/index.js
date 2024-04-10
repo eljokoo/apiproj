fce72b2df41b8955a22842cbbc39deafba6aa181
Eljo Kondi <67634728+ascendedkaito@users.noreply.github.com>
2024-04-10 17:06:35 -0400

    Final commenting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.scss';
 
 let num = 0;
 
+// Increase counter every second and update DOM element
 setInterval(() => {
   document.querySelector('#main').innerHTML = /* html */`<p>You've been on this page for ${num} seconds.</p>`;
   num += 1;
