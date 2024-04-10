cdac253be87ddc93a8162d70360a2272cdde4244
Eljo Kondi <67634728+ascendedkaito@users.noreply.github.com>
2024-04-09 23:43:01 -0400

    Ready for deployment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import './style.scss';
+
+let num = 0;
+
+setInterval(() => {
+  document.querySelector('#main').innerHTML = /* html */`<p>You've been on this page for ${num} seconds.</p>`;
+  num += 1;
+}, 1000);
